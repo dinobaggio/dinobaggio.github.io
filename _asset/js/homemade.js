00b029b97d23b7965118ee3a9adbbfc03aa0d86e
dinobaggio <dino.baggio007@gmail.com>
2017-09-24 06:07:19 +0700

    add animasi show

--- _asset/js/homemade.js
+++ _asset/js/homemade.js
@@ -22,7 +22,7 @@ function openNav() {
 
 $(document).ready(function(){
     $("#buttonPersonal").click(function(){
-        $("#personal").show();
+        $("#personal").show("slow");
         $("#mySkill").hide();
         $("#contact").hide();
         $("#buttonPersonal").removeClass("w3-theme-d3");
@@ -35,7 +35,7 @@ $(document).ready(function(){
     });
     $("#buttonSkill").click(function(){
         $("#personal").hide();
-        $("#mySkill").show();
+        $("#mySkill").show("slow");
         $("#contact").hide();
         $("#buttonPersonal").removeClass("w3-theme-d5");
         $("#buttonPersonal").addClass("w3-theme-d3");
@@ -48,7 +48,7 @@ $(document).ready(function(){
     $("#buttonContact").click(function(){
         $("#personal").hide();
         $("#mySkill").hide();
-        $("#contact").show();
+        $("#contact").show("slow");
         $("#buttonPersonal").removeClass("w3-theme-d5");
         $("#buttonPersonal").addClass("w3-theme-d3");
         $("#buttonContact").removeClass("w3-theme-d3");
@@ -56,4 +56,50 @@ $(document).ready(function(){
         $("#buttonSkill").removeClass("w3-theme-d5");
         $("#buttonSkill").addClass("w3-theme-d3");
     });
+
+    $("#profil").addClass("w3-animate-bottom");
+    document.getElementById('profil').style.display='';
+    setTimeout(function(){
+        $("#fotoProfil").show("slow");
+    },200);
+    setTimeout(function(){
+        $("#skill").addClass("w3-animate-bottom");
+        document.getElementById('skill').style.display='';
+    },300);
+    setTimeout(function(){
+        $("#tombol").addClass("w3-animate-bottom");
+        document.getElementById('tombol').style.display='';
+        
+    },500);
+    setTimeout(function(){
+        $("#isiTombol").show("slow");
+    },600);
+    setTimeout(function(){
+        $("#isi").addClass("w3-animate-right");
+        document.getElementById('isi').style.display='';
+    },800);
+    setTimeout(function(){
+        $("#personal").show("slow");
+        
+    },1000);
+    setTimeout(function(){
+        $("#buttonPersonal").removeClass("w3-theme-d3");
+        $("#buttonPersonal").addClass("w3-theme-d5");
+    },1200);
+    setTimeout(function(){
+        $("#footer").show("slow");
+    },900);
+    
+
+
+    /* $("#profil").show("slow"); */
+    /* 
+     
+    
+    setTimeout(function(){
+        $("#isi").show("slow");
+    },600);
+    setTimeout(function(){
+        $("#footer").show("slow");
+    },900);  */
 });
